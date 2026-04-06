56a1c579d90194cdfcc21f3e9c3443ea7e649666
Atul Himanshu <priyanshuprogrammer49@gmail.com>
2026-04-06 22:50:55 +0530

    test 1

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "http://localhost:5000/api",
-	// baseURL: "https://subarno-server.onrender.com/api",
+	// baseURL: "http://localhost:5000/api",
+	baseURL: "https://subarno-server.onrender.com/api",
 	withCredentials: true, // send cookies to the server
 });
 
